4f20fd2f76635b2a0d5fa27f8c904dcdd37d3fab
Borgar <borgar@borgar.net>
2011-10-19 18:25:49 +0000

    Nearly complete date formatting and parsing.
    The formatting and parsing implementaion is now more complete than
    most non-os implementations around.
    
    What is still missing:
    
    Week of year + day of week (other than ISO 8601). These are the only
    parts of the spec that require effort without any expectation of usage.
    
    Timezones are ignored. Formatter wasn't time-zone complete to begin with,
    though strangely not using UTC functions. I've locked it to consistent
    UTC output. The parser should eventually support reading basic offsets
    and shifting date accordingly.

--- src/text/DateFormat.js
+++ src/text/DateFormat.js
@@ -14,52 +14,53 @@
  * <li>%d - day of month [01,31] (zero padded).</li>
  * <li>%D - same as %m/%d/%y.</li>
  * <li>%e - day of month [ 1,31] (space padded).</li>
+ * <li>%f - milliseconds [000,999].</li>
+ * <li>%F - same as %Y-%m-%d.</li>
+ * <li>%g - ISO 8601 year without century [00,99].</li>
+ * <li>%G - ISO 8601 year with century.</li>
  * <li>%h - same as %b.</li>
  * <li>%H - hour (24-hour clock) [00,23] (zero padded).</li>
  * <li>%I - hour (12-hour clock) [01,12] (zero padded).</li>
+ * <li>%j - day number [1,366].</li>
+ * <li>%k - hour (24-hour clock) [0,23].</li>
+ * <li>%l - hour (12-hour clock) [1,12].</li>
  * <li>%m - month number [01,12] (zero padded).</li>
  * <li>%M - minute [0,59] (zero padded).</li>
  * <li>%n - newline character.</li>
- * <li>%p - locale's equivalent of a.m. or p.m.</li>
+ * <li>%p - locale's equivalent of a.m. or p.m. [AM,PM].</li>
+ * <li>%P - like %p but in lowercase [am,pm].</li>
+ * <li>%q - quarter of the year [1,4].</li>
+ * <li>%Q - milliseconds [000,999] (zero padded).</li>
  * <li>%r - same as %I:%M:%S %p.</li>
  * <li>%R - same as %H:%M.</li>
+ * <li>%s - number of seconds since unix Epoch.</li>
  * <li>%S - second [00,61] (zero padded).</li>
  * <li>%t - tab character.</li>
  * <li>%T - same as %H:%M:%S.</li>
+ * <li>%u - ISO 8601 weekday number [1,7].</li>
+ * <li>%U - week number [00,53].</li>
+ * <li>%V - ISO 8601 week number [01,53].</li>
+ * <li>%w - weekday number [0,6].</li>
+ * <li>%W - week number [00,53].</li>
  * <li>%x - same as %m/%d/%y.</li>
  * <li>%X - same as %I:%M:%S %p.</li>
  * <li>%y - year with century [00,99] (zero padded).</li>
  * <li>%Y - year including century.</li>
- * <li>%% - %.</li>
- *
- * </ul>The following conversion specifications are currently <i>unsupported</i>
- * for formatting:<ul>
- *
- * <li>%j - day number [1,366].</li>
- * <li>%u - weekday number [1,7].</li>
- * <li>%U - week number [00,53].</li>
- * <li>%V - week number [01,53].</li>
- * <li>%w - weekday number [0,6].</li>
- * <li>%W - week number [00,53].</li>
+ * <li>%z - time-zone hour offset from GMT.</li>
  * <li>%Z - timezone name or abbreviation.</li>
+ * <li>%% - %.</li>
  *
  * </ul>In addition, the following conversion specifications are currently
  * <i>unsupported</i> for parsing:<ul>
  *
  * <li>%a - day of week, either abbreviated or full name.</li>
  * <li>%A - same as %a.</li>
- * <li>%c - locale's appropriate date and time.</li>
- * <li>%C - century number.</li>
- * <li>%D - same as %m/%d/%y.</li>
- * <li>%I - hour (12-hour clock) [1,12].</li>
- * <li>%n - any white space.</li>
- * <li>%p - locale's equivalent of a.m. or p.m.</li>
- * <li>%r - same as %I:%M:%S %p.</li>
- * <li>%R - same as %H:%M.</li>
- * <li>%t - same as %n.</li>
- * <li>%T - same as %H:%M:%S.</li>
- * <li>%x - locale's equivalent to %m/%d/%y.</li>
- * <li>%X - locale's equivalent to %I:%M:%S %p.</li>
+ * <li>%A - same as %a.</li>
+ * <li>%U - week number [00,53].</li>
+ * <li>%w - weekday number [0,6].</li>
+ * <li>%W - week number [00,53].</li>
+ * <li>%z - time-zone hour offset from GMT.</li>
+ * <li>%Z - timezone name or abbreviation.</li>
  *
  * </ul>
  *
@@ -75,65 +76,66 @@
 pv.Format.date = function(pattern) {
   var pad = pv.Format.pad;
 
+  // expand shorthand notation
+  pattern = pattern.replace( /%F/g, '%Y-%m-%d' )
+                   .replace( /%[Dx]/g, '%m/%d/%y' )
+                   .replace( /%[Xr]/g, '%I:%M:%S %p' )
+                   .replace( /%R/g, '%H:%M' )
+                   .replace( /%T/g, '%H:%M:%S' )
+                   ;
+
   /** @private */
   function format(d) {
     return pattern.replace(/%[a-zA-Z0-9]/g, function(s) {
         switch (s) {
-          case '%a': return [
-              "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
-            ][d.getDay()];
-          case '%A': return [
-              "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-              "Saturday"
-            ][d.getDay()];
+          case '%a': return format.days[d.getUTCDay()].substring(0,3);
+          case '%A': return format.days[d.getUTCDay()];
           case '%h':
-          case '%b': return [
-              "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
-              "Oct", "Nov", "Dec"
-            ][d.getMonth()];
-          case '%B': return [
-              "January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"
-            ][d.getMonth()];
+          case '%b': return format.months[d.getUTCMonth()].substring(0,3);
+          case '%B': return format.months[d.getUTCMonth()];
           case '%c': return d.toLocaleString();
-          case '%C': return pad("0", 2, Math.floor(d.getFullYear() / 100) % 100);
-          case '%d': return pad("0", 2, d.getDate());
-          case '%x':
-          case '%D': return pad("0", 2, d.getMonth() + 1)
-                    + "/" + pad("0", 2, d.getDate())
-                    + "/" + pad("0", 2, d.getFullYear() % 100);
-          case '%e': return pad(" ", 2, d.getDate());
-          case '%H': return pad("0", 2, d.getHours());
-          case '%I':
-            var t = d.getHours() % 12;
-            return t ? pad("0", 2, t) : 12;
-          // TODO %j: day of year as a decimal number [001,366]
-          case '%m': return pad("0", 2, d.getMonth() + 1);
-          case '%M': return pad("0", 2, d.getMinutes());
+          case '%C': return pad("0", 2, Math.floor(d.getUTCFullYear() / 100) % 100);
+          case '%d': return pad("0", 2, d.getUTCDate());
+          // case %D: shorthand, see above
+          case '%f':
+          case '%Q': return pad("0", 3, d.getUTCMilliseconds());
+          case '%e': return pad(" ", 2, d.getUTCDate());
+          // case %F: shorthand, see above
+          case '%g': return pad("0", 2, format.isocalendar(d)[0] % 100);
+          case '%G': return pad("0", 4, format.isocalendar(d)[0]);
+          case '%H': return pad("0", 2, d.getUTCHours());
+          case '%I': return pad("0", 2, d.getUTCHours() % 12 || 12);
+          case '%j': return pad("0", 3, Math.floor(( d - Date.UTC( d.getUTCFullYear(), 0, 1 ) ) / 864e5 ) + 1);
+          case '%k': return d.getUTCHours();
+          case '%l': return d.getUTCHours() % 12 || 12;
+          case '%m': return pad("0", 2, d.getUTCMonth() + 1);
+          case '%M': return pad("0", 2, d.getUTCMinutes());
           case '%n': return "\n";
-          case '%p': return d.getHours() < 12 ? "AM" : "PM";
-          case '%T':
-          case '%X':
-          case '%r':
-            var h = d.getHours() % 12;
-            return (h ? pad("0", 2, h) : 12)
-                    + ":" + pad("0", 2, d.getMinutes())
-                    + ":" + pad("0", 2, d.getSeconds())
-                    + " " + (d.getHours() < 12 ? "AM" : "PM");
-          case '%R': return pad("0", 2, d.getHours()) + ":" + pad("0", 2, d.getMinutes());
-          case '%S': return pad("0", 2, d.getSeconds());
-          case '%Q': return pad("0", 3, d.getMilliseconds());
+          case '%p': return d.getUTCHours() < 12 ? "AM" : "PM";
+          case '%P': return d.getUTCHours() < 12 ? "am" : "pm";
+          case '%q': return Math.floor( d.getUTCMonth() / 3 ) + 1;
+          // case %r: shorthand, see above
+          // case %R: shorthand, see above
+          case '%s': return ~~( d / 1000 );
+          case '%S': return pad("0", 2, d.getUTCSeconds());
           case '%t': return "\t";
-          case '%u':
-            var w = d.getDay();
-            return w ? w : 1;
-          // TODO %U: week number (sunday first day) [00,53]
-          // TODO %V: week number (monday first day) [01,53] ... with weirdness
-          case '%w': return d.getDay();
-          // TODO %W: week number (monday first day) [00,53] ... with weirdness
-          case '%y': return pad("0", 2, d.getFullYear() % 100);
-          case '%Y': return d.getFullYear();
-          // TODO %Z: timezone name or abbreviation
+          // case %T: shorthand, see above
+          case '%u': return d.getUTCDay() || 7;
+          case '%U':
+            var doy = Math.floor(( d - Date.UTC( d.getUTCFullYear(), 0, 1 ) ) / 864e5 ) + 1;
+            return pad("0", 2, Math.floor((doy + 6 - d.getUTCDay()) / 7));
+          case '%V':
+            return pad("0", 2, format.isocalendar(d)[1]);
+          case '%w': return d.getUTCDay();
+          case '%W':
+            var doy = Math.floor(( d - Date.UTC( d.getUTCFullYear(), 0, 1 ) ) / 864e5 ) + 1;
+            return pad("0", 2, Math.floor((doy + 7 - (d.getUTCDay() || 7)) / 7));
+          // case %x: shorthand, see above
+          // case %X: shorthand, see above
+          case '%y': return pad("0", 2, d.getUTCFullYear() % 100);
+          case '%Y': return d.getUTCFullYear();
+          case '%z': return "+0000";
+          case '%Z': return "UTC";
           case '%%': return "%";
         }
         return s;
@@ -150,6 +152,127 @@ pv.Format.date = function(pattern) {
    */
   format.format = format;
 
+  // Create expression from format pattern and register callbacks for 
+  // each field in the format pattern.
+  var fields = [function(){}],
+      parse_re = pv.Format.re(pattern).replace(/(%[a-zA-Z0-9]|\s+)/g, function(s) {
+        // flexible whitespace
+        if ( /^\s/.test(s) ) { return "\\s+"; }
+        switch (s) {
+          case '%A': // TODO %U: weekday [monday,sunday]
+          case '%a': // TODO %U: weekday [mon,sun]
+            fields.push(function(){});
+            return "([A-Za-z]+)";
+          case '%b': // [jan,dec]
+          case '%h': // [jan,dec]
+          case '%B': // [january,december]
+            fields.push(function(x,d) {
+              x = x.toLowerCase();
+              var m = -1, mn = this.months, l = x.length;
+              while ( ++m < 12 ) {
+                if ( mn[m].substr(0,l).toLowerCase() == x ) {
+                  return d.month = m;
+                }
+              }
+            });
+            return "([A-Za-z]+)";
+          case '%C': // [00,99]
+            fields.push(function(x,d) { d.year = x * 100 + d.year % 100; });
+            return "(\\d\\d)";
+          case '%e': // [ 1,31]
+            fields.push(function(x,d) { d.date = Number(x); });
+            return "\\s?([12]\\d|3[01]|[1-9])";
+          case '%d': // [01,31]
+            fields.push(function(x,d) { d.date = Number(x); });
+            return "\\s?(0[1-9]|[12]\\d|3[01])";
+          case '%f': // [000,999]([000,999])?
+          case '%Q': // [000,999]([000,999])?
+            fields.push(function(x,d) { d.msec = parseFloat('0.' + x) * 1000; });
+            return "(\\d{3,6})";
+          case '%g': // [00,99] iso year
+            fields.push(function(x,d) {
+              x = Number(x);
+              d.isoyear = x + (x < 69 ? 2000 : 1900);
+            });
+            return "(\\d\\d)";
+          case '%G': // [0000,9999] iso year
+            fields.push(function(x,d) { d.isoyear = Number(x); });
+            return "(\\d{4})";
+          case '%I': // [01,12]
+            fields.push(function(x,d) { d.hour = Number(x); });
+            return "(0\\d|[12]\\d)";
+          case '%H': // [00,23]
+            fields.push(function(x,d) { d.hour = Number(x); });
+            return "(0\\d|1\\d|2[0-4])";
+          case '%j': // [001,366]
+            fields.push(function(x,d) { d.month = 0; d.date = Number(x);
+            });
+            return "(\\d{3})";
+          case '%l': // [ 1,12]
+            fields.push(function(x,d) { d.hour = Number(x); });
+            return "\\s?(\\d|1[012])";
+          case '%m': // [01,12]
+            fields.push(function(x,d) { d.month = Number(x)-1; });
+            return "(0[1-9]|1[012])";
+          case '%M': // [00,59]
+            fields.push(function(x,d) { d.minute = Number(x); });
+            return "(0\\d|[1-5]\\d)";
+          case '%p': // [AM,PM]
+          case '%P': // [am,pm]
+            fields.push(function(x,d) {
+              d.hour = (d.hour % 12) + (/^a\.?m$/i.test(x) ? 0 : 12);
+            });
+            return "([ap]\\.?m|[AP]\\.?M)\\.?";
+          case '%q': // [1,4]
+            fields.push(function(x,d) {
+              d.date = 1;
+              d.month = (Number(x) - 1) * 3;
+            });
+            return "(\\d)";
+          case '%s':
+            fields.push(function(x,d) { d.ts = x * 1000; });
+            return "(-?\\d+)";
+          case '%S':
+            fields.push(function(x,d) { d.second = Number(x); });
+            return "(0\\d|[1-5]\\d|6[01])";
+          case "%n": // "\n"
+          case "%t": // "\t"
+            return "\\s*";
+          case "%U": // TODO %U: week number [00,53]
+            fields.push(function(){});
+            return "(0\\d|[1-4]\\d|5[0-3])";
+          case "%u": // iso weekday [1,7]
+            fields.push(function (x,d) { d.isoday = Number(x); });
+            return '([1-7])';
+          case '%V':
+            fields.push(function(x,d) { d.isoweek = Number(x); });
+            return "(0[1-9]|[1-4]\\d|5[0-3])";
+          case "%w": // weekday [0,6]
+            fields.push(function(){});
+            return '([0-6])';
+          case '%W': // TODO %W: week number [00,53]
+            fields.push(function(){});
+            return "(0\\d|[1-4]\\d|5[0-3])";
+          case '%y': // [00,99]
+            fields.push(function(x,d) {
+              x = Number(x);
+              d.year = x + (x < 69 ? 2000 : 1900);
+            });
+            return "(\\d\\d)";
+          case '%Y': // [0000,9999]
+            fields.push(function(x,d) { d.year = Number(x); });
+            return "(\\d{4})";
+          case '%z':
+          case '%Z':
+            fields.push(function(){});
+            return "([+-]\\d{4}|GMT|[CEP]ST|UTC|Z)";
+          case '%%': // "%"
+            return "%";
+        }
+        return s;
+      });
+
+
   /**
    * Parses a date from a string using the associated formatting pattern.
    *
@@ -159,90 +282,53 @@ pv.Format.date = function(pattern) {
    * @returns {Date} the parsed date.
    */
   format.parse = function(s) {
-    var year = 1970, month = 0, date = 1, hour = 0, minute = 0, second = 0;
-    var fields = [function() {}];
-
-    /* Register callbacks for each field in the format pattern. */
-    var re = pv.Format.re(pattern).replace(/%[a-zA-Z0-9]/g, function(s) {
-        switch (s) {
-          // TODO %a: day of week, either abbreviated or full name
-          // TODO %A: same as %a
-          case '%b':
-            fields.push(function(x) { month = {
-                  Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5, Jul: 6, Aug: 7,
-                  Sep: 8, Oct: 9, Nov: 10, Dec: 11
-                }[x]; });
-            return "([A-Za-z]+)";
-          case '%h':
-          case '%B':
-            fields.push(function(x) { month = {
-                  January: 0, February: 1, March: 2, April: 3, May: 4, June: 5,
-                  July: 6, August: 7, September: 8, October: 9, November: 10,
-                  December: 11
-                }[x]; });
-            return "([A-Za-z]+)";
-          // TODO %c: locale's appropriate date and time
-          // TODO %C: century number[0,99]
-          case '%e':
-          case '%d':
-            fields.push(function(x) { date = x; });
-            return "([0-9]+)";
-          // TODO %D: same as %m/%d/%y
-          case '%I':
-          case '%H':
-            fields.push(function(x) { hour = x; });
-            return "([0-9]+)";
-          // TODO %j: day number [1,366]
-          case '%m':
-            fields.push(function(x) { month = x - 1; });
-            return "([0-9]+)";
-          case '%M':
-            fields.push(function(x) { minute = x; });
-            return "([0-9]+)";
-          // TODO %n: any white space
-          // TODO %p: locale's equivalent of a.m. or p.m.
-          case '%p': // TODO this is a hack
-            fields.push(function(x) {
-              if (hour == 12) {
-                if (x == "am") hour = 0;
-              } else if (x == "pm") {
-                hour = Number(hour) + 12;
-              }
-            });
-            return "(am|pm)";
-          // TODO %r: %I:%M:%S %p
-          // TODO %R: %H:%M
-          case '%S':
-            fields.push(function(x) { second = x; });
-            return "([0-9]+)";
-          // TODO %t: any white space
-          // TODO %T: %H:%M:%S
-          // TODO %U: week number [00,53]
-          // TODO %w: weekday [0,6]
-          // TODO %W: week number [00, 53]
-          // TODO %x: locale date (%m/%d/%y)
-          // TODO %X: locale time (%I:%M:%S %p)
-          case '%y':
-            fields.push(function(x) {
-                x = Number(x);
-                year = x + (((0 <= x) && (x < 69)) ? 2000
-                    : (((x >= 69) && (x < 100) ? 1900 : 0)));
-              });
-            return "([0-9]+)";
-          case '%Y':
-            fields.push(function(x) { year = x; });
-            return "([0-9]+)";
-          case '%%':
-            fields.push(function() {});
-            return "%";
-        }
-        return s;
-      });
-
-    var match = s.match(re);
-    if (match) match.forEach(function(m, i) { fields[i](m); });
-    return new Date(year, month, date, hour, minute, second);
+    var match = s.match(parse_re);
+    if ( match ) {
+      var d = {
+        msec: 0,
+        second: 0,
+        minute: 0,
+        hour: 0,
+        date: 1,
+        month: 0,
+        year: 1970,
+        isoyear: null,
+        isoweek: null,
+        isoday: null,
+        ts: null,
+      };
+      match.forEach(function(m, i) { fields[i].call(format, m, d); });
+      // have timestamp
+      if ( d.ts !== null ) { return new Date(d.ts); }
+      // convert iso
+      if ( d.isoyear !== null && d.isoweek !== null ) {
+        var iy = new Date(Date.UTC(d.isoyear, 0, 3));
+        d.year = d.isoyear;
+        d.month = 0;
+        d.date = 3 - iy.getUTCDay() + (d.isoweek - 1) * 7 + (d.isoday || 1);
+      }
+      return new Date(Date.UTC(d.year, d.month, d.date, d.hour, d.minute, d.second, d.msec));
+    }
+    return new Date('?'); // Invalid Date
   };
+
+  // allow overwriting the month/days
+  // FIXME: replace with a translation hook
+  format.months = [ "January", "February", "March", "April", "May", "June", "July",
+                    "August", "September", "October", "November", "December" ];
+  format.days   = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+                    "Saturday" ];
+
+  /** @private */
+  format.isocalendar = function ( dt ) {
+    var d = dt.getUTCDay(),
+        t = new Date( dt * 1 );
+    t.setDate( t.getDate() - ((d + 6) % 7) + 3 );
+    var iso_year = t.getUTCFullYear(),
+        w = Math.floor( (t.getTime() - Date.UTC(iso_year, 0, 1, -6)) / 864e5 );
+    // return a 3-tuple, (ISO year, ISO week number, ISO weekday).
+    return [ iso_year, 1+Math.floor(w/7), d||7 ];
+  }
 
   return format;
 };
